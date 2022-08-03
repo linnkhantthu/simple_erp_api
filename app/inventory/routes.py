b265041e0c1f7cdf6f4b44eb85fff9305993ddf8
linnkhantthu <linnkhantthu1999mdy@gmail.com>
2022-08-03 23:55:48 +0630

    connected with the app through inventory route

--- app/inventory/routes.py
+++ app/inventory/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, jsonify, request
 from app.inventory.models import Product
 from app.users.models import User
 
@@ -6,22 +6,146 @@ inventory = Blueprint('inventory', __name__)
 
 
 @inventory.route('/inventory', methods=['POST'])
-def login():
+def products():
+    dummyProductList = [
+        {
+            "id": 1,
+            "productName": "Hat",
+            "contains": 100,
+            "unit": "PCS",
+            "price": 2500.0,
+            "qty": 200,
+        },
+        {
+            "id": 2,
+            "productName": "Shoes",
+            "contains": 50,
+            "unit": "PCS",
+            "price": 25000.0,
+            "qty": 150,
+        },
+        {
+            "id": 1,
+            "productName": "iPad",
+            "contains": 1,
+            "unit": "PCS",
+            "price": 1100000.0,
+            "qty": 5,
+        },
+        {
+            "id": 1,
+            "productName": "iPad",
+            "contains": 1,
+            "unit": "PCS",
+            "price": 1100000.0,
+            "qty": 5,
+        },
+        {
+            "id": 1,
+            "productName": "iPad",
+            "contains": 1,
+            "unit": "PCS",
+            "price": 1100000.0,
+            "qty": 5,
+        },
+        {
+            "id": 1,
+            "productName": "iPad",
+            "contains": 1,
+            "unit": "PCS",
+            "price": 1100000.0,
+            "qty": 5,
+        },
+        {
+            "id": 1,
+            "productName": "iPad",
+            "contains": 1,
+            "unit": "PCS",
+            "price": 1100000.0,
+            "qty": 5,
+        },
+        {
+            "id": 1,
+            "productName": "iPad",
+            "contains": 1,
+            "unit": "PCS",
+            "price": 1100000.0,
+            "qty": 5,
+        },
+        {
+            "id": 1,
+            "productName": "iPad",
+            "contains": 1,
+            "unit": "PCS",
+            "price": 1100000.0,
+            "qty": 5,
+        },
+        {
+            "id": 1,
+            "productName": "iPad",
+            "contains": 1,
+            "unit": "PCS",
+            "price": 1100000.0,
+            "qty": 5,
+        },
+        {
+            "id": 1,
+            "productName": "iPad",
+            "contains": 1,
+            "unit": "PCS",
+            "price": 1100000.0,
+            "qty": 5,
+        },
+        {
+            "id": 1,
+            "productName": "iPad",
+            "contains": 1,
+            "unit": "PCS",
+            "price": 1100000.0,
+            "qty": 5,
+        },
+        {
+            "id": 1,
+            "productName": "iPad",
+            "contains": 1,
+            "unit": "PCS",
+            "price": 1100000.0,
+            "qty": 5,
+        },
+        {
+            "id": 1,
+            "productName": "iPad",
+            "contains": 1,
+            "unit": "PCS",
+            "price": 1100000.0,
+            "qty": 5,
+        },
+        {
+            "id": 1,
+            "productName": "iPad",
+            "contains": 1,
+            "unit": "PCS",
+            "price": 1100000.0,
+            "qty": 5,
+        },
+    ]
     data = request.get_json()
     user = User.query.filter_by(mail=data['mail']).first()
     if user:
-        response = []
+        response = {
+            "status": True,
+            "data": [],
+        }
         products = Product.query.filter_by(owner=user)
-        for product in product:
-            response.append({
-                "status": True,
-                "data": {"id": product.id,
-                         "productName": product.productName,
-                         "contains": product.contains,
-                         "unit": product.unit,
-                         "price": product.price,
-                         "qty": product.qty}
-            })
+        for product in dummyProductList:
+            response["data"].append({"id": product['id'],
+                                     "productName": product['productName'],
+                                     "contains": product['contains'],
+                                     "unit": product['unit'],
+                                     "price": product['price'],
+                                     "qty": product['qty']}
+                                    )
+        return jsonify(response)
     else:
         response = {"status": False, "data": {
             "message": "You don't have access to this page"}}
