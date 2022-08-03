e6962c12a5599f24243105dd321c9d05d3fe267e
linnkhantthu <linnkhantthu1999mdy@gmail.com>
2022-08-03 16:41:25 +0630

    added product model

--- app/users/models.py
+++ app/users/models.py
@@ -3,10 +3,24 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    firstName = db.Column(db.String(60))
-    lastName = db.Column(db.String(60))
-    mail = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
+    firstName = db.Column(db.String(60), nullable=False)
+    lastName = db.Column(db.String(60), nullable=False)
+    mail = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    product = db.relationship('Product', backref='owner', lazy=True)
 
     def __repr__(self):
         return f"<User {self.mail}>"
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    productName = db.Column(db.String(60), nullable=False)
+    contains = db.Column(db.String(60), nullable=False)
+    unit = db.Column(db.String(150), unique=True, nullable=False)
+    price = db.Column(db.String(150), nullable=False)
+    qty = db.Column(db.String(150), default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Product {self.productName}>"
